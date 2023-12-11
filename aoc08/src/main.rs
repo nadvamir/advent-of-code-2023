6b2e333d84b9f998fa2bb6c785ea51d913dd2a31
Maksim Solovjov <himaxx2@gmail.com>
2023-12-10 19:10:36 -0500

    Solve 08: Haunted Wasteland, part 2

--- aoc08/src/main.rs
+++ aoc08/src/main.rs
@@ -1,4 +1,3 @@
-use core::num;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -41,14 +40,101 @@ fn solve(lines: &[String]) -> usize {
 }
 
 // ----------------------------------------------------------------------------
+fn calc_individual_paths(
+    start: &String,
+    directions: &String,
+    network: &HashMap<String, HashMap<char, String>>,
+) -> (usize, Vec<usize>) {
+    let mut current = start;
+    let mut visited: HashMap<(&String, usize), usize> = Default::default();
+    let mut num_iterations: usize = 0;
+    let mut possible_endpoints: Vec<usize> = Default::default();
+
+    let next_step = |it| {
+        directions
+            .chars()
+            .nth(it % directions.len())
+            .unwrap()
+    };
+
+    while !visited.contains_key(&(current, num_iterations % directions.len())) {
+        visited.insert((current, num_iterations % directions.len()), num_iterations);
+        if current.ends_with('Z') {
+            possible_endpoints.push(num_iterations);
+        }
+
+        let step = next_step(num_iterations);
+        current = &network[current][&step];
+        num_iterations += 1;
+    }
+
+    let pre_cycle = *visited.get(&(current, num_iterations % directions.len())).unwrap();
+    let cycle_len = num_iterations - pre_cycle;
+    // assume that we always have a cycle, and thus filter out all possibilities that precede it
+    possible_endpoints = possible_endpoints.into_iter().filter(|e| *e >= pre_cycle).collect();
+    (cycle_len, possible_endpoints)
+}
+
 fn solve2(lines: &[String]) -> usize {
-    0
+    let directions = lines[0].to_string();
+    let network = parse_network(&lines[2..]);
+
+    let current: Vec<&String> = network.keys().filter(|k| k.ends_with('A')).collect();
+    let playouts: Vec<(usize, Vec<usize>)> = current
+        .iter()
+        .map(|c| calc_individual_paths(c, &directions, &network))
+        .collect();
+
+    let (max_cycle, candidates) = playouts.iter().max_by(|(a, _), (b, _)| a.cmp(b)).unwrap();
+
+    let terminated = |num_iteration| {
+        let any_terminate = |max_cycle, candidates: &Vec<usize>| {
+            candidates.iter().any(|c| (num_iteration - c) % max_cycle == 0)
+        };
+        playouts.iter().all(|(max_cycle, candidates)| any_terminate(max_cycle, candidates))
+    };
+
+    let mut iteration: usize = 0;
+    let mut candidate: usize = 0;
+    while !terminated(candidates.get(candidate).unwrap() + iteration * max_cycle) {
+        candidate += 1;
+        if candidate == candidates.len() {
+            candidate = 0;
+            iteration += 1;
+        }
+    }
+
+    candidates.get(candidate).unwrap() + iteration * max_cycle
 }
 
 // ----------------------------------------------------------------------------
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_calc_individual_paths() {
+        let input = r"LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+";
+        let lines: Vec<String> = input.lines().map(|line| line.to_string()).collect();
+        let directions = lines[0].to_string();
+        let network = parse_network(&lines[2..]);
+        let (cycle_len, _) =
+            calc_individual_paths(&"11A".to_string(), &directions, &network);
+        assert_eq!(cycle_len, 2);
+        let (cycle_len, _) =
+            calc_individual_paths(&"22A".to_string(), &directions, &network);
+        assert_eq!(cycle_len, 6);
+    }
 
     #[test]
     fn test_solution1_1() {
@@ -82,32 +168,20 @@ ZZZ = (ZZZ, ZZZ)
 
     #[test]
     fn test_solution2_1() {
-        let input = r"RL
+        let input = r"LR
 
-AAA = (BBB, CCC)
-BBB = (DDD, EEE)
-CCC = (ZZZ, GGG)
-DDD = (DDD, DDD)
-EEE = (EEE, EEE)
-GGG = (GGG, GGG)
-ZZZ = (ZZZ, ZZZ)
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
 ";
         let lines: Vec<String> = input.lines().map(|line| line.to_string()).collect();
         let result = solve2(&lines);
-        assert_eq!(result, 0);
-    }
-
-    #[test]
-    fn test_solution2_2() {
-        let input = r"LLR
-
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)
-";
-        let lines: Vec<String> = input.lines().map(|line| line.to_string()).collect();
-        let result = solve2(&lines);
-        assert_eq!(result, 0);
+        assert_eq!(result, 6);
     }
 }
 
