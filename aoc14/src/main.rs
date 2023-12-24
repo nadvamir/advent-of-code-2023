8c7eb1b96843bcbc4fc47f9dee5f6336801d1565
Maksim Solovjov <himaxx2@gmail.com>
2023-12-23 22:46:11 -0500

    Solve 14: Parabolic Reflector Dish, part 2

--- aoc14/src/main.rs
+++ aoc14/src/main.rs
@@ -1,59 +1,85 @@
-use core::num;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-fn tilt_row(canvas: &mut Vec<String>, j: usize, i: usize) -> usize {
-    if i < canvas.len() && canvas[i].as_bytes()[j] == b'#' {
-        return i + 1;
-    }
-    let mut row = i;
-    let mut num_stones = 0;
-
-    while row < canvas.len() {
-        let item = canvas[row].as_bytes()[j];
-        if item == b'O' {
-            num_stones += 1;
+fn tilt_row(row: &mut String, cache: &mut HashMap<String, String>) {
+    if let Some(memoised) = cache.get(row) {
+        for (i, c) in memoised.bytes().enumerate() {
+            unsafe {
+                row.as_bytes_mut()[i] = c;
+            }
         }
-        else if item == b'#' {
-            break;
-        }
-        row += 1;
+        return;
     }
 
-    let mut stone_row = i;
-    while num_stones > 0 {
-        unsafe {
-            canvas[stone_row].as_bytes_mut()[j] = b'O';
+    let key = row.clone();
+
+    let mut i = 0;
+    while i < row.len() {
+        let begin_it = i;
+        let mut n_stones = 0;
+
+        // find how many movable rows we have
+        while i < row.len() {
+            match row.as_bytes()[i] {
+                b'O' => n_stones += 1,
+                b'#' => break,
+                _ => {}
+            }
+            i += 1;
         }
-        num_stones -= 1;
-        stone_row += 1;
+
+        // fill out stones followed by empty spaces
+        for fill_i in begin_it..begin_it + n_stones {
+            unsafe {
+                row.as_bytes_mut()[fill_i] = b'O';
+            }
+        }
+        for fill_i in begin_it + n_stones..i {
+            unsafe {
+                row.as_bytes_mut()[fill_i] = b'.';
+            }
+        }
+
+        i += 1;
     }
 
-    while stone_row < row {
-        unsafe {
-            canvas[stone_row].as_bytes_mut()[j] = b'.';
-        }
-        stone_row += 1;
-    }
-
-    return row;
+    cache.insert(key, row.clone());
 }
 
-fn tilt(lines: &[String]) -> Vec<String> {
-    let mut canvas: Vec<String> = lines.iter().map(|s| (*s).clone()).collect();
-    for j in 0..lines[0].len() {
-        let mut i = 0;
-        while i < canvas.len() {
-            i = tilt_row(&mut canvas, j, i);
+fn tilt(canvas: &mut Vec<String>, cache: &mut HashMap<String, String>) {
+    for i in 0..canvas.len() {
+        tilt_row(&mut canvas[i], cache);
+    }
+}
+
+fn rotate_cw(canvas: &mut Vec<String>) {
+    let n = canvas.len();
+    for i in 0..n / 2 {
+        for j in 0..n / 2 {
+            unsafe {
+                let c = canvas[i].as_bytes_mut()[j];
+                canvas[i].as_bytes_mut()[j] = canvas[n - j - 1].as_bytes_mut()[i];
+                canvas[n - j - 1].as_bytes_mut()[i] = canvas[n - i - 1].as_bytes_mut()[n - j - 1];
+                canvas[n - i - 1].as_bytes_mut()[n - j - 1] = canvas[j].as_bytes_mut()[n - i - 1];
+                canvas[j].as_bytes_mut()[n - i - 1] = c;
+            }
         }
     }
-    canvas
 }
 
 fn solve(lines: &[String]) -> usize {
-    let tilted = tilt(lines);
-    tilted
+    let mut canvas: Vec<String> = lines.iter().map(|s| (*s).clone()).collect();
+    let mut cache: HashMap<String, String> = Default::default();
+
+    rotate_cw(&mut canvas);
+    rotate_cw(&mut canvas);
+    rotate_cw(&mut canvas);
+    tilt(&mut canvas, &mut cache);
+    rotate_cw(&mut canvas);
+
+    canvas
         .iter()
         .enumerate()
         .map(|(i, line)| line.chars().filter(|c| *c == 'O').count() * (lines.len() - i))
@@ -61,8 +87,53 @@ fn solve(lines: &[String]) -> usize {
 }
 
 // ----------------------------------------------------------------------------
+fn tilt_full(canvas: &mut Vec<String>, cache: &mut HashMap<String, String>) {
+    rotate_cw(canvas);
+    rotate_cw(canvas);
+    rotate_cw(canvas);
+    tilt(canvas, cache);
+    rotate_cw(canvas);
+    tilt(canvas, cache);
+    rotate_cw(canvas);
+    tilt(canvas, cache);
+    rotate_cw(canvas);
+    tilt(canvas, cache);
+    rotate_cw(canvas);
+    rotate_cw(canvas);
+}
+
 fn solve2(lines: &[String]) -> usize {
-    0
+    let mut canvas: Vec<String> = lines.iter().map(|s| (*s).clone()).collect();
+    let mut cache: HashMap<String, String> = Default::default();
+    let mut cycle_cache: HashMap<String, usize> = Default::default();
+
+    let mut i = 0;
+
+    let (cycle_start, cycle_len) = loop {
+        let flattened = canvas.concat();
+        if let Some(cycle_start) = cycle_cache.get(&flattened) {
+            break (cycle_start, i - cycle_start);
+        }
+        cycle_cache.insert(flattened, i);
+
+        tilt_full(&mut canvas, &mut cache);
+        i += 1;
+    };
+
+    let nth_iter = cycle_start + (1000000000 - cycle_start) % cycle_len;
+    let (final_canvas, _) = cycle_cache.iter().find(|(_, v)| **v == nth_iter).unwrap();
+
+    final_canvas
+        .chars()
+        .enumerate()
+        .map(|(i, c)| {
+            if c == 'O' {
+                canvas.len() - i / canvas.len()
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 
 // ----------------------------------------------------------------------------
@@ -90,11 +161,56 @@ O.#..O.#.#
 
     #[test]
     fn test_solution2() {
-        let input = r"
+        let input = r"O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
 ";
         let lines: Vec<String> = input.lines().map(|line| line.to_string()).collect();
         let result = solve2(&lines);
-        assert_eq!(result, 0);
+        assert_eq!(result, 64);
+    }
+
+    #[test]
+    fn test_full_cycle() {
+        let input = r"O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
+";
+        let lines: Vec<String> = input.lines().map(|line| line.to_string()).collect();
+
+        let mut canvas: Vec<String> = lines.iter().map(|s| (*s).clone()).collect();
+        let mut cache: HashMap<String, String> = Default::default();
+
+        tilt_full(&mut canvas, &mut cache);
+
+        let output = r".....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....
+";
+        let lines: Vec<String> = output.lines().map(|line| line.to_string()).collect();
+        let expected_canvas: Vec<String> = lines.iter().map(|s| (*s).clone()).collect();
+        assert_eq!(canvas, expected_canvas);
     }
 }
 
